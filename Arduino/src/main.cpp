03ff56672376522ec94c4ce4fffd462aa5fc2748
svenar-nl <github@svenar.nl>
2022-04-20 15:37:43 +0200

    Implemented better serial communication manager

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,50 +1,32 @@
 #include <Arduino.h>
+#include "SerialManager.h"
 
-char buf[80];
-
-int readline(int readch, char *buffer, int len) {
-  static int pos = 0;
-  int rpos;
-
-  if (readch > 0) {
-    switch (readch) {
-      case '\r': // Ignore CR
-        break;
-      case '\n': // Return on new-line
-        rpos = pos;
-        pos = 0;  // Reset position index ready for next time
-        return rpos;
-      default:
-        if (pos < len - 1) {
-          buffer[pos++] = readch;
-          buffer[pos] = 0;
-        }
-    }
-  }
-  return 0;
-}
+SerialManager serialManager = SerialManager();
 
 void setup() {
-  Serial.begin(115200);
+  serialManager.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  if (readline(Serial.read(), buf, 80) > 0) {
-    switch (buf[0]) {
-      case '0':
+  serialManager.update();
+
+  if (serialManager.isAvailable()) {
+    int num = serialManager.getData().toInt();
+    String text = serialManager.getData();
+
+    if (isDigit(text.charAt(0))) {
+      if (num == 0) {
         digitalWrite(LED_BUILTIN, LOW);
         Serial.println("ACK");
-        break;
-
-      case '1':
+      } else if (num == 1) {
         digitalWrite(LED_BUILTIN, HIGH);
         Serial.println("ACK");
-        break;
-
-      default:
+      } else {
         Serial.println("NACK");
-        break;
+      }
+    } else {
+      Serial.println("NACK");
     }
   }
 }

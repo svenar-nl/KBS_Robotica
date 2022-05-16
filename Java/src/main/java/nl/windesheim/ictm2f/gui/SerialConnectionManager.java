d253564e2da3bdfd2a43adea44e3a932555c3096
svenar-nl <github@svenar.nl>
2022-05-16 23:23:36 +0200

    Added serial communication indicator

--- Java/src/main/java/nl/windesheim/ictm2f/gui/SerialConnectionManager.java
+++ Java/src/main/java/nl/windesheim/ictm2f/gui/SerialConnectionManager.java
@@ -3,7 +3,6 @@ package nl.windesheim.ictm2f.gui;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
-import java.nio.charset.StandardCharsets;
 import java.awt.event.ActionEvent;
 import java.util.List;
 
@@ -24,7 +23,7 @@ public class SerialConnectionManager extends JPanel {
 
     private GUIThemes guiTheme;
     private Dimension screenDimension;
-    private JButton connectionButton, btn0, btn1, startButton, settingsButton;
+    private JButton connectionButton, startButton, settingsButton;
 
     public SerialConnectionManager(Dimension screenDimension, GUIThemes guiTheme) {
         this.screenDimension = new Dimension(screenDimension.getX(), 50);
@@ -51,8 +50,8 @@ public class SerialConnectionManager extends JPanel {
         this.connectionButton.setBackground(this.guiTheme.getTheme().getBackgroundColor());
         this.connectionButton.setForeground(this.guiTheme.getTheme().getTextColor());
 
-        this.startButton = new JButton("Start");
-        this.startButton.setBounds(620, 10, 120, 30);
+        this.startButton = new JButton("Run order");
+        this.startButton.setBounds(540, 10, 120, 30);
 
         this.startButton.setBorderPainted(false);
         this.startButton.setFocusPainted(false);
@@ -60,18 +59,9 @@ public class SerialConnectionManager extends JPanel {
         this.startButton.setBackground(this.guiTheme.getTheme().getBackgroundColor());
         this.startButton.setForeground(this.guiTheme.getTheme().getTextColor());
 
-        this.btn0 = new JButton("0");
-        this.btn0.setBounds(480, 10, 60, 30);
-        this.btn0.setBackground(this.guiTheme.getTheme().getBackgroundColor());
-        this.btn0.setForeground(this.guiTheme.getTheme().getTextColor());
-        this.btn1 = new JButton("1");
-        this.btn1.setBounds(550, 10, 60, 30);
-        this.btn1.setBackground(this.guiTheme.getTheme().getBackgroundColor());
-        this.btn1.setForeground(this.guiTheme.getTheme().getTextColor());
-
         Icon settingsIcon = new ImageIcon(getClass().getResource("/gear.png"));
         this.settingsButton = new JButton(settingsIcon);
-        this.settingsButton.setBounds(750,10,30,30);
+        this.settingsButton.setBounds(750, 10, 30, 30);
 
         this.settingsButton.setBackground(this.guiTheme.getTheme().getBackgroundColor());
         this.settingsButton.setForeground(this.guiTheme.getTheme().getTextColor());
@@ -82,8 +72,6 @@ public class SerialConnectionManager extends JPanel {
         this.add(jComboBox);
         this.add(this.connectionButton);
         this.add(this.startButton);
-        this.add(this.btn0);
-        this.add(this.btn1);
         this.add(this.settingsButton);
 
         this.connectionButton.addActionListener(new ActionListener() {
@@ -93,7 +81,8 @@ public class SerialConnectionManager extends JPanel {
                     if (Main.getInstance().getSerialManager().isConnected()) {
                         Main.getInstance().getSerialManager().disconnect();
                     } else {
-                        Main.getInstance().getSerialManager().connect(jComboBox.getItemAt(jComboBox.getSelectedIndex()));
+                        Main.getInstance().getSerialManager()
+                                .connect(jComboBox.getItemAt(jComboBox.getSelectedIndex()));
                     }
                     repaint();
                 } catch (SerialPortInvalidPortException ex) {
@@ -107,62 +96,68 @@ public class SerialConnectionManager extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 try {
                     Main.getInstance().getSolver().SolveDynamic();
-                    GUIManager.getControlPanel().repaint();         // repain
+                    GUIManager.getControlPanel().repaint(); // repain
                 } catch (SerialPortInvalidPortException ex) {
                     Logger.exception(ex);
                 }
             }
         });
 
-        this.btn0.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Main.getInstance().getSerialManager().write("0\r\n");
+        // this.btn0.addActionListener(new ActionListener() {
+        // @Override
+        // public void actionPerformed(ActionEvent e) {
+        // Main.getInstance().getSerialManager().write("0\r\n");
 
-                if (Main.getInstance().getSerialManager().isConnected()) {
-                    try {
-                        byte[] readBuffer = new byte[Main.getInstance().getSerialManager().getPort().bytesAvailable()];
-                        int numRead = Main.getInstance().getSerialManager().getPort().readBytes(readBuffer,
-                                readBuffer.length);
-                        if (numRead > 0) {
-                            System.out.println(new String(readBuffer, StandardCharsets.UTF_8).substring(0, numRead - 2)
-                                    + " (" + numRead + " bytes)");
-                        }
-                    } catch (NegativeArraySizeException nase) {
-                        Main.getInstance().getSerialManager().disconnect();
-                        Logger.warning("Serial device disconnected!");
-                        repaint();
-                    }
-                }
-            }
-        });
+        // if (Main.getInstance().getSerialManager().isConnected()) {
+        // try {
+        // byte[] readBuffer = new
+        // byte[Main.getInstance().getSerialManager().getPort().bytesAvailable()];
+        // int numRead =
+        // Main.getInstance().getSerialManager().getPort().readBytes(readBuffer,
+        // readBuffer.length);
+        // if (numRead > 0) {
+        // System.out.println(new String(readBuffer,
+        // StandardCharsets.UTF_8).substring(0, numRead - 2)
+        // + " (" + numRead + " bytes)");
+        // }
+        // } catch (NegativeArraySizeException nase) {
+        // Main.getInstance().getSerialManager().disconnect();
+        // Logger.warning("Serial device disconnected!");
+        // repaint();
+        // }
+        // }
+        // }
+        // });
+
+        // this.btn1.addActionListener(new ActionListener() {
+        // @Override
+        // public void actionPerformed(ActionEvent e) {
+        // Main.getInstance().getSerialManager().write("1\r\n");
+
+        // if (Main.getInstance().getSerialManager().isConnected()) {
+        // try {
+        // byte[] readBuffer = new
+        // byte[Main.getInstance().getSerialManager().getPort().bytesAvailable()];
+        // int numRead =
+        // Main.getInstance().getSerialManager().getPort().readBytes(readBuffer,
+        // readBuffer.length);
+        // if (numRead > 0) {
+        // System.out.println(new String(readBuffer,
+        // StandardCharsets.UTF_8).substring(0, numRead - 2)
+        // + " (" + numRead + " bytes)");
+        // }
+        // } catch (NegativeArraySizeException nase) {
+        // Main.getInstance().getSerialManager().disconnect();
+        // repaint();
+        // }
+        // }
+        // }
+        // });
 
         this.settingsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 GUIManager.getSettings().display();
-            }
-        });
-
-        this.btn1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Main.getInstance().getSerialManager().write("1\r\n");
-
-                if (Main.getInstance().getSerialManager().isConnected()) {
-                    try {
-                        byte[] readBuffer = new byte[Main.getInstance().getSerialManager().getPort().bytesAvailable()];
-                        int numRead = Main.getInstance().getSerialManager().getPort().readBytes(readBuffer,
-                                readBuffer.length);
-                        if (numRead > 0) {
-                            System.out.println(new String(readBuffer, StandardCharsets.UTF_8).substring(0, numRead - 2)
-                                    + " (" + numRead + " bytes)");
-                        }
-                    } catch (NegativeArraySizeException nase) {
-                        Main.getInstance().getSerialManager().disconnect();
-                        repaint();
-                    }
-                }
             }
         });
     }
@@ -181,10 +176,23 @@ public class SerialConnectionManager extends JPanel {
         g.drawLine(0, this.screenDimension.getY() - 1, this.screenDimension.getX(), this.screenDimension.getY() - 1);
 
         g.setColor(this.guiTheme.getTheme().getTextColor());
-
         g.setFont(new Font("Arial", Font.BOLD, 18));
-        g.drawString("Select device:", 20, this.screenDimension.getY() - 20);
+        g.drawString("Select device:", 15, this.screenDimension.getY() - 20);
 
         this.connectionButton.setText(Main.getInstance().getSerialManager().isConnected() ? "Disconnect" : "Connect");
+
+        g.setColor(this.guiTheme.getTheme().getTextColor());
+        g.setFont(new Font("Arial", Font.BOLD, 13));
+        g.drawString("RX", 480, 20);
+        g.drawString("TX", 510, 20);
+
+        g.setColor(!Main.getInstance().getSerialManager().isConnected() ? this.guiTheme.getTheme().getGridPointColor()
+                : (!Main.getInstance().getSerialManager().pingRX() ? this.guiTheme.getTheme().getGridPathColor()
+                        : this.guiTheme.getTheme().getGridFetchedPointColor()));
+        g.fillOval(482, this.screenDimension.getY() - 22, 12, 12);
+        g.setColor(!Main.getInstance().getSerialManager().isConnected() ? this.guiTheme.getTheme().getGridPointColor()
+                : (!Main.getInstance().getSerialManager().pingTX() ? this.guiTheme.getTheme().getGridPathColor()
+                        : this.guiTheme.getTheme().getGridFetchedPointColor()));
+        g.fillOval(512, this.screenDimension.getY() - 22, 12, 12);
     }
 }

17752b05562917251895c89be68938c2f144999d
svenar-nl <github@svenar.nl>
2022-05-18 02:14:27 +0200

    Added My-Es-Que-Ell connection information to settings panel

--- Java/src/main/java/nl/windesheim/ictm2f/gui/settings/SettingsPanel.java
+++ Java/src/main/java/nl/windesheim/ictm2f/gui/settings/SettingsPanel.java
@@ -17,9 +17,10 @@ public class SettingsPanel extends JPanel {
 
     private SettingsFrame parentFrame;
     private GUIThemes currentTheme;
-    private JLabel storagelabel;
+    private JLabel storagelabel, titleMysqlSettings, lblMysqlHost, lblMysqlDB, lblMysqlUser, lblMysqlPass;
     private JButton buttonToggleTheme, buttonSaveSettings;
     private JComboBox<String> selectStorage;
+    private JTextField mysqlIP, mysqlPort, mysqlDatabase, mysqlUsername, mysqlPassword;
     private Icon iconThemeLight, iconThemeDark;
     private boolean saveQueued;
 
@@ -53,10 +54,42 @@ public class SettingsPanel extends JPanel {
         this.selectStorage.setForeground(this.currentTheme.getTheme().getTextColor());
         this.selectStorage.setSelectedItem(Main.getInstance().getDatabase().getType());
 
+        this.titleMysqlSettings = new JLabel("MySQL connection settings");
+        this.lblMysqlHost = new JLabel("Host");
+        this.lblMysqlDB = new JLabel("DB");
+        this.lblMysqlUser = new JLabel("User");
+        this.lblMysqlPass = new JLabel("Pass");
+        this.mysqlIP = new JTextField("127.0.0.1");
+        this.mysqlPort = new JTextField("3306");
+        this.mysqlDatabase = new JTextField("db");
+        this.mysqlUsername = new JTextField("username");
+        this.mysqlPassword = new JTextField("password");
+
+        this.titleMysqlSettings.setBounds(30, 80, (int) startDimensions.getWidth() - 52, 20);
+        this.lblMysqlHost.setBounds(10, 100, (int) startDimensions.getWidth() - 52, 20);
+        this.lblMysqlDB.setBounds(10, 130, (int) startDimensions.getWidth() - 52, 20);
+        this.lblMysqlUser.setBounds(10, 160, (int) startDimensions.getWidth() - 52, 20);
+        this.lblMysqlPass.setBounds(10, 190, (int) startDimensions.getWidth() - 52, 20);
+        this.mysqlIP.setBounds(50, 100, (int) startDimensions.getWidth() / 3 - 10, 25);
+        this.mysqlPort.setBounds(50 + (int) startDimensions.getWidth() / 3, 100, (int) (startDimensions.getWidth() / 2 - startDimensions.getWidth() / 3), 25);
+        this.mysqlDatabase.setBounds(50, 130, (int) startDimensions.getWidth() / 2, 25);
+        this.mysqlUsername.setBounds(50, 160, (int) startDimensions.getWidth() / 2, 25);
+        this.mysqlPassword.setBounds(50, 190, (int) startDimensions.getWidth() / 2, 25);
+
         this.add(this.buttonToggleTheme);
         this.add(this.buttonSaveSettings);
         this.add(this.storagelabel);
         this.add(this.selectStorage);
+        this.add(this.titleMysqlSettings);
+        this.add(this.lblMysqlHost);
+        this.add(this.lblMysqlDB);
+        this.add(this.lblMysqlUser);
+        this.add(this.lblMysqlPass);
+        this.add(this.mysqlIP);
+        this.add(this.mysqlPort);
+        this.add(this.mysqlDatabase);
+        this.add(this.mysqlUsername);
+        this.add(this.mysqlPassword);
 
         setupListeners();
     }
@@ -119,5 +152,33 @@ public class SettingsPanel extends JPanel {
         this.storagelabel.setForeground(this.currentTheme.getTheme().getTextColor());
         this.selectStorage.setBackground(this.currentTheme.getTheme().getBackgroundColor());
         this.selectStorage.setForeground(this.currentTheme.getTheme().getTextColor());
+
+        this.titleMysqlSettings.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.lblMysqlHost.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.lblMysqlDB.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.lblMysqlUser.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.lblMysqlPass.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.mysqlIP.setBackground(this.currentTheme.getTheme().getBackgroundColor());
+        this.mysqlIP.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.mysqlPort.setBackground(this.currentTheme.getTheme().getBackgroundColor());
+        this.mysqlPort.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.mysqlDatabase.setBackground(this.currentTheme.getTheme().getBackgroundColor());
+        this.mysqlDatabase.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.mysqlUsername.setBackground(this.currentTheme.getTheme().getBackgroundColor());
+        this.mysqlUsername.setForeground(this.currentTheme.getTheme().getTextColor());
+        this.mysqlPassword.setBackground(this.currentTheme.getTheme().getBackgroundColor());
+        this.mysqlPassword.setForeground(this.currentTheme.getTheme().getTextColor());
+
+        boolean mysqlVisible = selectStorage.getItemAt(selectStorage.getSelectedIndex()).equalsIgnoreCase("MySQL");
+        this.titleMysqlSettings.setVisible(mysqlVisible);
+        this.lblMysqlHost.setVisible(mysqlVisible);
+        this.lblMysqlDB.setVisible(mysqlVisible);
+        this.lblMysqlUser.setVisible(mysqlVisible);
+        this.lblMysqlPass.setVisible(mysqlVisible);
+        this.mysqlIP.setVisible(mysqlVisible);
+        this.mysqlPort.setVisible(mysqlVisible);
+        this.mysqlDatabase.setVisible(mysqlVisible);
+        this.mysqlUsername.setVisible(mysqlVisible);
+        this.mysqlPassword.setVisible(mysqlVisible);
     }
 }

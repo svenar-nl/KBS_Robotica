fe1d47492358ed79597261debf0f2f9e9b518af7
Gurvink <joostvangerrevink0@gmail.com>
2022-05-19 10:36:46 +0200

    robot

---
+++ Arduino/src/LegoMotor/LegoMotor.ino
@@ -0,0 +1,82 @@
+#include <Servo.h>
+
+int M2R = 6;
+int M2S = 7;
+int M1R = 4;
+int M1S = 5;
+
+Servo s1;
+int s1Pos = 0;
+
+void setup() {
+  for(int i=0; i<=13; i++){
+    pinMode(i, OUTPUT);
+  }
+  pinMode(A0, INPUT);
+  Serial.begin(115200);
+  s1.attach(13);
+}
+
+void Motor2(int pwm, boolean links){
+  analogWrite(M2R, pwm);
+  if(links){
+    digitalWrite(M2S, HIGH);
+  } else {
+    digitalWrite(M2S, LOW);
+  }
+}
+
+
+
+void Motor1(int pwm, boolean links){
+  analogWrite(M1R, pwm);
+  if(links){
+    digitalWrite(M1S, HIGH);
+  } else {
+    digitalWrite(M1S, LOW);
+  }
+}
+
+void duw(){
+  Serial.println("duw");
+  unsigned long tijd = millis();
+  while (millis() - tijd <= 800){
+  Motor2(255, false);
+  }
+  tijd = millis();
+  while(millis() - tijd <= 925){
+  Motor2(255, true);
+  }
+  Motor2(0, true);
+}
+
+void calib() {
+  Serial.println("Calibrating...");
+  s1.write(90);
+  unsigned long tijd = millis();
+  while (millis() - tijd <= 3000){
+  Motor2(125, false);
+  }
+  Motor2(0,true);
+  s1.write(0);
+  return;
+}
+
+void loop() {
+  if (Serial.available() > 0){
+    int t = Serial.read();
+    if(t == '0'){
+      Motor2(255,false);
+      delay(250);
+      Motor2(0,false);  
+    } else if(t == '1'){
+      Motor2(255, true);
+      delay(250);
+      Motor2(0, true);
+    } else if (t == '2'){
+      duw();
+    } else if (t == '3') {
+      calib();
+    }
+  }
+}

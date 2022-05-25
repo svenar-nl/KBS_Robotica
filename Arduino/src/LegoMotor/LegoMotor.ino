0a9315c304fced403f92f82d489e32f5e800e0e6
Gurvink <joostvangerrevink0@gmail.com>
2022-05-25 10:57:37 +0200

    heen en weer

--- Arduino/src/LegoMotor/LegoMotor.ino
+++ Arduino/src/LegoMotor/LegoMotor.ino
@@ -29,54 +29,66 @@ void Motor2(int pwm, boolean links){
 
 
 void Motor1(int pwm, boolean links){
-  analogWrite(M1R, pwm);
+  analogWrite(M1S, pwm);
   if(links){
-    digitalWrite(M1S, HIGH);
+    digitalWrite(M1R, HIGH);
   } else {
-    digitalWrite(M1S, LOW);
+    digitalWrite(M1R, LOW);
   }
 }
 
 void duw(){
   Serial.println("duw");
   unsigned long tijd = millis();
-  while (millis() - tijd <= 800){
-  Motor2(255, false);
+  while (millis() - tijd <= 450){
+  Motor2(125, false);
   }
   tijd = millis();
-  while(millis() - tijd <= 925){
-  Motor2(255, true);
+  while(millis() - tijd <= 575){
+  Motor2(125, true);
   }
   Motor2(0, true);
 }
 
-void calib() {
-  Serial.println("Calibrating...");
-  s1.write(90);
-  unsigned long tijd = millis();
-  while (millis() - tijd <= 3000){
-  Motor2(125, false);
+void Gaan(int schap){
+  for(int i=0; i<schap; i++){
+    unsigned long tijd = millis();
+    while (millis() - tijd <= 150){
+      Motor1(50,false);
+      Motor1(0,false);
+    }
   }
-  Motor2(0,true);
-  s1.write(0);
-  return;
+  duw();
+  for(int i=0; i<schap; i++){
+  unsigned long tijd = millis();
+    while (millis() - tijd <= 150){
+      Motor1(50,true);
+      Motor1(0,true);
+    }
+   }
 }
 
 void loop() {
   if (Serial.available() > 0){
     int t = Serial.read();
     if(t == '0'){
-      Motor2(255,false);
-      delay(250);
-      Motor2(0,false);  
+      Gaan(0);
     } else if(t == '1'){
-      Motor2(255, true);
-      delay(250);
-      Motor2(0, true);
+      Gaan(1);
     } else if (t == '2'){
-      duw();
-    } else if (t == '3') {
-      calib();
+      Gaan(2);
+    } else if(t == '3'){
+      Gaan(3);
+    } else if(t == '4'){
+      Gaan(4);
+    } else if (t == '5'){
+      Motor1(255,false);
+      delay(100);
+      Motor1(0,false);
+    } else if (t=='6'){
+      Motor1(255,true);
+      delay(100);
+      Motor1(0,true);
     }
   }
 }

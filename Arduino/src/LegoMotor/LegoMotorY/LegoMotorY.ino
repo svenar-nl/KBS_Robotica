60daf702a9b87cd537f924be3c2780ad2e78501e
Niels van Dam <s1169063@student.windesheim.nl>
2022-05-30 13:13:39 +0200

    Copied X axis code from Robot-Duw

--- Arduino/src/LegoMotor/LegoMotorY/LegoMotorY.ino
+++ Arduino/src/LegoMotor/LegoMotorY/LegoMotorY.ino
@@ -77,6 +77,7 @@ void loop() {
             Serial.println("Ending X axis control.");
            } else if (t == '1') {
             Serial.println("Sending X to vak 1.");
+            mySerial.write('1');
            }
          }
       }
